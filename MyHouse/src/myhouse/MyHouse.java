15b9e16c544020120988b2fc1fb76c1d53d19d04
Zeroslash666 <ja43793666@gmail.com>
2020-03-01 18:05:33 -0600

    Iniciando Proyecto

---
+++ MyHouse/src/myhouse/MyHouse.java
@@ -0,0 +1,19 @@
+package myhouse;
+
+public class MyHouse {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Iniciando");    
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
